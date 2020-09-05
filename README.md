65adb23516909e69163b9df927dea322e6a97500
Danny Caballero <danny.caballero@gmail.com>
2020-09-05 08:43:09 -0400

    inital repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# CMSE 202 Fall 2020 Slides Repo
---
+++ bin/convertSlides.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+Written by Danny Caballero
+5 Sept 2020
+
+This script will read a notebook filename and convert it to slides. It keeps the notebook and slides in differnt folders. Any images used in the notebook will be copied over to the slides folder.
+'''
+
+import sys
+from subprocess import call
+
+NOTEBOOK = sys.argv[1]
+NOTEBOOKDIR = 'notebooks/'
+HTMLDIR = 'html/'
+IMAGESDIR = 'images'
+
+ext = NOTEBOOK.index(".ipynb")
+newext = ".slides.html"
+
+SLIDES = NOTEBOOK[:ext] + newext
+
+call(["jupyter-nbconvert", NOTEBOOK, "--to", "slides"])
+
+call(["mv", SLIDES, HTMLDIR])
+call(["cp", "-r", NOTEBOOKDIR + IMAGESDIR, HTMLDIR])
